fd8b1e584b98645ebc7eefbd0bf0fae0c74d4c1f
EyeVorE <subplot_05tracer@icloud.com>
2024-04-29 02:26:04 -0500

    created sample logos in each shape.

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -8,7 +8,7 @@ describe("Circle class", () => {
     expect(typeof circle).toEqual("object");
   });
 
-  test("when i call the render method i expect it to return the svg string", () => {
+  test("When I call the render method I expect it to return the svg string.", () => {
     expect(circle.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx='150' cy='100' r='80' fill= 'a' /><text x="150" y="125" font-size="60" text-anchor="middle" fill="b">c</text></svg>`)
   });
 
--- tests/square.test.js
+++ tests/square.test.js
@@ -8,7 +8,7 @@ describe("Square class", () => {
     expect(typeof square).toEqual("object");
   });
 
-  test("when i call the render method i expect it to return the svg string", () => {
+  test("When I call the render method I expect it to return the svg string.", () => {
     expect(square.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect width="150" height="150" x="100" y="50" fill= 'a' /><text x="175" y="140" font-size="60" text-anchor="middle" fill="b">c</text></svg>`)
   });
 
